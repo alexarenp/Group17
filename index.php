af4d23656f09a0c6788ffbeb7e2a7962f5ccb427
Alexander Fisher <fishayy@Alexanders-MacBook-Air-105.local>
2026-04-06 17:35:41 -0600

    finished 3.1

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<section class="background-index">
 
     <nav class="navbar">
-        <h2 class="logo">(Insert logo we create)</h2>
+        <img src="pictures/color_map.jpeg" alt="Logo" class="logo">
         <ul class="nav-links">
             <li><a href="color.php">Color</a></li>
             <li><a href="print.php">Print</a></li>
@@ -17,9 +18,17 @@
         </ul>
     </nav>
 
-    <nav class="hero">
-        <h1>(Logo name)</h1>
-</nav>
+    <nav class="padding-wrap-logo">
+        <img src = "pictures/color_map.jpeg" alt="Logo" class="logo2" >
+    </nav>
 
+    <h1>Welcome to Color Map Generator</h1>
+
+    <div class="features">
+        <div class="feature"><h3>Color Mapping:</h3> <p>Generate structured color grids to organize and label your selections clearly.</p> </div>
+        <div class="feature"><h3>Optimized Output:</h3> <p>Generate printable sheets that remain clear and readable in any format.</p></div>
+        <div class="feature"><h3>Fast Setup:</h3> <p>Input your parameters and instantly produce a structured color map.</p></div>
+    </div>
+</nav>
 </body>
 </html>
