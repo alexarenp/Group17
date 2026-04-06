f119a1aed728b082291af312084bc1f188d56219
Alexander Fisher <fishayy@Alexanders-MacBook-Air-105.local>
2026-04-06 15:27:19 -0600

    checkpoint for changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav class="navbar">
+        <h2 class="logo">(Insert logo we create)</h2>
+        <ul class="nav-links">
+            <li><a href="color.php">Color</a></li>
+            <li><a href="print.php">Print</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+
+    <nav class="hero">
+        <h1>(Logo name)</h1>
+</nav>
+
+</body>
+</html>
