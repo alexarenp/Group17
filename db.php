2d2e6354bd0ff11c71c6c0e742b4bf72dfc8cbd7
Camacho Flores <diony121@dover.cs.colostate.edu>
2026-05-01 20:09:45 -0600

    Database set up and instuctions for indiviaual peson

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = 'helmi.cs.colostate.edu'; 
-$username = 'diony121';
-$password = 'juniorlluni1212';
-$database = 'diony121';
-
-$conn = new mysqli($host, $username, $password, $database);
-?>
